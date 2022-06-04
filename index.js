9abb51a76d851a2e6167842b16a21eece0e583b8
grarizki <28901232+grarizki@users.noreply.github.com>
2022-06-04 19:14:44 +0700

    add tugas callback

--- index.js
+++ index.js
@@ -6,15 +6,26 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (name, sortFn) => {
+  let nama = [];
+  let semuaNama = sortFn(name);
+  for (let i = 0; i < semuaNama.length; i++) {
+    nama.push(i + 1 + ". " + semuaNama[i]);
+  }
+  return nama;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (name) => {
+  return name.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (name) => {
+  return name.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
